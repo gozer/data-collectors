dd0b9ee260baa8c56e4d095c41e72d13e7be22e8
Aaron Wirick <awirick@mozilla.com>
2017-10-19 12:47:06 -0700

    Fix formatting (flake8)

--- collectors/__init__.py
+++ collectors/__init__.py
@@ -1,14 +1,16 @@
 from collectors.adjust import adjust_cmd
-from collectors.redash import redash_cmd
 from collectors.main import cli
+from collectors.redash import redash_cmd
 
 # Add subcommands here to get around weird inheritance issues
 cli.add_command(adjust_cmd)
 cli.add_command(redash_cmd)
 
+
 # Workaroud to pass an empty context.obj
 def main():
     cli(obj={})
+
 
 __all__ = ['main']
 
